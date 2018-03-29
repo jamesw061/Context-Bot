fb802498c8692e6c6678874c6e06e149c580f24e
jamesw061 <spacesavage.jw@gmail.com>
2018-03-29 22:29:30 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import praw
+import os
+import string
+import twitterbio
+import re
+import csv
+import datetime
+
+reddit = praw.Reddit('contextbot1')
+subreddit = reddit.subreddit('ukpolitics')
+twitbio = twitterbio
+session = twitbio.auth()
+threadlist = list()
+newlist = list()
+log = list()
+
+log.append('------------------------')
+log.append(' ')
+log.append(datetime.datetime.now())
+with open("replylist.csv", 'r+', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter =',')   
+    for row in reader:
+        threadlist.append(row)
+log.append('posted list loaded')
+for submission in subreddit.new(limit=10):   
+    if "twitter.com/" not in submission.url or [submission.id] in threadlist:
+        print(submission.id + ' - skipped')
+        log.append(submission.id + ' - skipped')
+        continue
+    with open('boilerplate.txt', 'r') as file1:
+        data = file1.read()
+    log.append('New thread')
+    sn = re.search(r".com/(\w+)", submission.url).group(1)
+    bio = twitbio.getBio(session, sn)
+    snip = ""
+    snip = re.search(r"[\w\.-]+@[\w\.-]+", bio)
+    if snip :bio = bio.replace(snip.group(0), " - ")
+    data = data.replace("%BIO%", bio)
+    try:
+        submission.reply(data)
+        with open("replylist.csv", 'a', newline='') as csvfile:
+            wr = csv.writer(csvfile, dialect='excel', delimiter = ',')
+            wr.writerow([submission.id])
+        log.append(data)
+        print(data)
+    except praw.exceptions.APIException as exc:
+        log.append(exc.message)
+with open("redditlog.log", 'a', newline='') as logfile:
+    wr = csv.writer(logfile)
+    for row in log:
+        wr.writerow([row])
+    wr.writerow('')
---
+++ twitterbio.py
@@ -0,0 +1,18 @@
+import tweepy
+import configparser
+
+config = configparser.ConfigParser()
+config.read('tweepy.ini')
+
+def auth():
+    auth = tweepy.OAuthHandler(config['auth']['oId'], config['auth']['oSecret'])
+    auth.set_access_token(config['auth']['aId'], config['auth']['aSecret'])
+    return tweepy.API(auth)
+
+def getBio( session , screenname ):   
+    usr = session.get_user(screenname)
+    vtxt = "^unverified"
+    if usr.verified:
+        vtxt = "^verified"    
+    return 'Twitter Name: ['+ usr.name +'](https://twitter.com/' + usr.screen_name + ')' + vtxt + ' Reach: ' + str(usr.followers_count) + '\n' + '\n' + 'Bio: ' + usr.description
+        
